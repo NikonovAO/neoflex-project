14542d3b305dbc33b9dd6b6bf88453e0ac162449
NikonovAO <nikonow@bk.ru>
2022-06-10 20:19:53 +0400

    fix: add folder registrationPage

---
+++ claim-processing/src/pages/registrationPage/registrationPage.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './registrationPage.scss'
+import logoBig from "../../assets/svg/logo/logo-big.svg";
+import Footer from "../../components/footer/footer";
+
+function RegistrationPage() {
+  return (
+    <>
+      <main className='registration-page'>
+        <div className='block-img'>
+          <img src = {require("../../assets/image/claims-analysis.png")} className="claims-analysis" alt="claims-analysis" />
+        </div>
+        <div className='block-registration'>
+          <img src = { logoBig } className="logo-big" alt="logo" />
+          <div className="test-block"></div>
+          <span>Проверка шрифтов</span>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default RegistrationPage;
