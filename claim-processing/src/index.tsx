8ea539a3540db6d2871306b55382c9c9ef64a82c
NikonovAO <nikonow@bk.ru>
2022-06-10 13:29:38 +0400

    feat: add normalize.scss

--- claim-processing/src/index.tsx
+++ claim-processing/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../src/styles/normalize.scss'
 import '../src/styles/fonts.scss'
 import './index.scss';
 import RegistrationPage from './pages/registrationPage/registrationPage';
