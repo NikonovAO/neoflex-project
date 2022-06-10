a54d3f7e8d412a5d4f6fc8140059848f71c8e3fc
NikonovAO <nikonow@bk.ru>
2022-06-10 13:25:36 +0400

    fix: move fonts to a separate style

--- claim-processing/src/index.tsx
+++ claim-processing/src/index.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '../src/styles/fonts.scss'
 import './index.scss';
 import RegistrationPage from './pages/registrationPage/registrationPage';
-import './assets/fonts/arial/arial-regular.ttf';
-import './assets/fonts/inter/Inter-Regular.ttf';
-import './assets/fonts/inter/Inter-Bold.ttf';
-import './assets/fonts/inter/Inter-Medium.ttf';
-import './assets/fonts/roboto/Roboto-Regular.ttf';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
